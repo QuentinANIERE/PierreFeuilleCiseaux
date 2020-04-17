00c934f38fcf3c88b101f126917e9c14111c8ce3
Quentin ANIERE <quentin.aniere@gmail.com>
2020-04-17 22:03:06 +0200

    Ajout d'un fonction qui permet de rejouer

--- PierreFeuilleCiseaux.py
+++ PierreFeuilleCiseaux.py
@@ -36,6 +36,22 @@ def choixUser():
 
     return choixDeUser
 
+#Fonction qui demande à l'utilisateur si il veut rejouer 
+def rejouer():
+    print() #On laisse un ligne vide
+    choix = input("Voulez vous rejouer ? (Oui|Non) \n>>> ")
+
+    if choix == "Oui" or  choix == "oui" or  choix == "o":
+        jeu()
+
+    elif choix == "Non" or choix == "non" or choix == "n":
+        print("Merci d'avoir joué, à bientôt !")
+        input("Appuyer sur entrée pour quitter")
+
+    else:
+        print("Votre choix n'est pas valide !")
+        rejouer()
+
 #Fonction qui contient le jeu en lui même
 def jeu():
     #On inscrit le choix de l'user dans des variables afin qu'elle ne changent à chaque demande
@@ -45,23 +61,32 @@ def jeu():
     #En cas d'égalité
     if  choixDeUser == choixDeOrdi:
         print("Égalité !")
+        rejouer()
 
     #Bloc if si le choix de l'ordi est "Pierre"
     if choixDeOrdi == "Pierre" and choixDeUser == "Feuille" and choixDeUser != choixDeOrdi:
         print("L'ordinateur à choisi", choixDeOrdi + ". Vous avez gagné !")
+        rejouer()
     elif choixDeOrdi == "Pierre" and choixDeUser != "Feuille" and choixDeUser != choixDeOrdi:
         print("L'ordinateur à choisi", choixDeOrdi +". L'ordinateur a gagné...")
+        rejouer()
 
     #Bloc if si le choix de l'ordi est "Feuille"
     if choixDeOrdi == "Feuille" and choixDeUser == "Ciseaux" and choixDeUser != choixDeOrdi:
        print("L'ordinateur à choisi", choixDeOrdi + ". Vous avez gagné !")
+       rejouer()
     elif choixDeOrdi == "Feuille" and choixDeUser != "Ciseaux" and choixDeUser != choixDeOrdi:
         print("L'ordinateur à choisi", choixDeOrdi +". L'ordinateur a gagné...")
+        rejouer()
 
     #Bloc if si le choix de l'ordi est "Ciseaux"
     if choixDeOrdi == "Ciseaux" and choixDeUser == "Pierre" and choixDeUser != choixDeOrdi:
         print("L'ordinateur à choisi", choixDeOrdi + ". Vous avez gagné !")
+        rejouer()
     elif choixDeOrdi == "Ciseaux" and choixDeUser != "Pierre" and choixDeUser != choixDeOrdi:
         print("L'ordinateur à choisi", choixDeOrdi +". L'ordinateur a gagné...")
+        rejouer()
 
 jeu()
+
+
