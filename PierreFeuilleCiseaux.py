fc99b92d5f8038566885209e4b40cfe0e9eb1aab
Quentin ANIERE <quentin.aniere@gmail.com>
2020-04-17 20:43:27 +0200

    Création de deux fonctions qui permettent de déterminer le choix de l'ordinateur et celui de l'utilisateur. Le choix de l'utilisateur est vérifié

--- PierreFeuilleCiseaux.py
+++ PierreFeuilleCiseaux.py
@@ -1,7 +1,6 @@
 from random import randint
 
-
-#Arrivé dans le jeu
+#Message de bienvenue
 print('Bienvenue dans le jeu du "Pierre, Feuille, Ciseaux" !')
 print() #Saut de ligne
 
@@ -9,5 +8,22 @@ print() #Saut de ligne
 def choixOrdi():
     choixPossibles = ["Pierre", "Feuille", "Ciseaux"]
     indexChoix = randint(0, 3)
-    choixOrdi = choixPossibles[indexChoix]
+    choixDeOrdi = choixPossibles[indexChoix]
+    return choixDeOrdi
 
+#Fonction qui demande à l'user son choix et vérifie si l'entrée est valide
+def choixUser():
+    global choixDeUser
+    choixDeUser = input("Que voulez faire ? (Pierre, Feuille, Ciseaux ?)\n>>> ")
+    #On vérifie que l'entrée de l'user est correcte 
+    listeEntresCorrectes = ["Pierre", "pierre", "p", "Feuille", "feuille", "f", "Ciseaux", "ciseaux", "ciseau","Ciseau", "c"]
+    if choixDeUser not in listeEntresCorrectes:
+    #On vérifie si l'entrée de l'utilisateur n'est pas dans la liste des choix possibles
+        print("Votre choix n'est pas valide !")
+        print()
+        choixUser()
+
+choixUser()
+
+#On affiche le résultat
+print("Vous avez choisi", choixDeUser)
