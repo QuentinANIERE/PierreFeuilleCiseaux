4e7572d7d4665ad37db89560b341c21ff94534f1
Quentin ANIERE <quentin.aniere@gmail.com>
2020-04-17 20:55:09 +0200

    Résolution d'un bug dans la fonction du choix de l'ordi (Index aléatoire trop grand) et formatage de l'entrée de l'user (pierre ==> Pierre | c ==> Ciseaux)

--- PierreFeuilleCiseaux.py
+++ PierreFeuilleCiseaux.py
@@ -7,7 +7,7 @@ print() #Saut de ligne
 #Fonction qui détermine le choix de l'ordi
 def choixOrdi():
     choixPossibles = ["Pierre", "Feuille", "Ciseaux"]
-    indexChoix = randint(0, 3)
+    indexChoix = randint(0, 2)
     choixDeOrdi = choixPossibles[indexChoix]
     return choixDeOrdi
 
@@ -15,15 +15,26 @@ def choixOrdi():
 def choixUser():
     global choixDeUser
     choixDeUser = input("Que voulez faire ? (Pierre, Feuille, Ciseaux ?)\n>>> ")
+
     #On vérifie que l'entrée de l'user est correcte 
     listeEntresCorrectes = ["Pierre", "pierre", "p", "Feuille", "feuille", "f", "Ciseaux", "ciseaux", "ciseau","Ciseau", "c"]
+    listeEntresValide = ["Pierre", "Feuille", "Ciseaux"] #Cette liste contient les entrées pouvant être comparées à celle de l'ordi
+
     if choixDeUser not in listeEntresCorrectes:
     #On vérifie si l'entrée de l'utilisateur n'est pas dans la liste des choix possibles
         print("Votre choix n'est pas valide !")
         print()
         choixUser()
+    
+    elif choixDeUser not in listeEntresValide:
+    #Si l'entrée de l'utilisateur n'est pas au bon format
+        if choixDeUser == "pierre" or choixDeUser == "p":
+            choixDeUser = "Pierre"
+        elif choixDeUser == "feuille" or choixDeUser == "f":
+            choixDeUser = "Feuille"
+        else: 
+            choixDeUser = "Ciseaux"
 
-choixUser()
+#choixUser()
 
-#On affiche le résultat
-print("Vous avez choisi", choixDeUser)
+#Fonction qui compare le choix de l'ordi et de l'user
