45391704272e8b499e3134806f855b1a58aedcf2
Quentin ANIERE <quentin.aniere@gmail.com>
2020-04-18 12:40:51 +0200

    Ajout des deux premier boutons dans la GUI

--- PierreFeuilleCiseaux.py
+++ PierreFeuilleCiseaux.py
@@ -1,7 +1,16 @@
 import os
 import time
 import platform
+from tkinter import *
 from random import randint
+
+#On défini la fênetre du jeu
+fen = Tk()
+fen.geometry("450x175")
+fen.resizable(width=False, height=False)
+fen.title("Pierre, Feuille, Ciseaux !")
+fen.configure(bg="grey")
+fen.iconbitmap(bitmap="medias/icone.ico")
 
 #On récupére le score dans le ficher sauvegarde.txt
 if os.path.isfile("sauvegarde.txt"):
@@ -10,19 +19,44 @@ if os.path.isfile("sauvegarde.txt"):
 else:
     score = 0
 
-#Fonction qui permet d'effacer la console
-def clear():
-    if platform.system() == "Windows":
-    #Si le script est executé sous windows
-        os.system("cls")
-    elif platform.system() == "Linux":
-    #Si le script est executé sous Linux
-        os.system("clear")
+#Lecture de photos
+photoPierre = PhotoImage(file="medias/pierre.png")
+photoFeuille = PhotoImage(file="medias/feuille.png")
+photoCiseaux = PhotoImage(file="medias/ciseaux.png")
 
-#Message de bienvenue
-print('Bienvenue dans le jeu du "Pierre, Feuille, Ciseaux" !')
-time.sleep(1.5)
-clear()
+cadre = Frame(fen, bg="Grey")
+cadre.pack(side=BOTTOM, pady=10)     
+
+def clicpierre(p):
+    print("Pierre !")
+
+pierre = Label(fen, bg="grey", image=photoPierre)
+pierre.pack(side="left", padx=50)
+pierre.bind('<Button-1>', clicpierre) 
+
+def clicfeuille(p):
+    print("Feuille !")
+
+feuille = Label(fen, bg="grey", image=photoFeuille)
+feuille.pack(side=LEFT, padx=10)
+feuille.bind('<Button-1>', clicfeuille) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Fonction qui détermine le choix de l'ordi
 def choixOrdi():
@@ -31,69 +65,18 @@ def choixOrdi():
     choixDeOrdi = choixPossibles[indexChoix]
     return choixDeOrdi
 
-#Fonction qui demande à l'user son choix et vérifie si l'entrée est valide
-def choixUser():
-    choixDeUser = input("Que voulez faire ? (Pierre, Feuille, Ciseaux ?)\n>>> ")
-    clear()
+#On vérifie que l'entrée de l'user est correcte 
+listeEntresCorrectes = ["Pierre", "pierre", "p", "Feuille", "feuille", "f", "Ciseaux", "ciseaux", "ciseau","Ciseau", "c"]
+listeEntresValide = ["Pierre", "Feuille", "Ciseaux"] #Cette liste contient les entrées pouvant être comparées à celle de l'ordi
 
-    #On vérifie que l'entrée de l'user est correcte 
-    listeEntresCorrectes = ["Pierre", "pierre", "p", "Feuille", "feuille", "f", "Ciseaux", "ciseaux", "ciseau","Ciseau", "c"]
-    listeEntresValide = ["Pierre", "Feuille", "Ciseaux"] #Cette liste contient les entrées pouvant être comparées à celle de l'ordi
-
-    if choixDeUser not in listeEntresCorrectes:
-    #On vérifie si l'entrée de l'utilisateur n'est pas dans la liste des choix possibles
-        print("Votre choix n'est pas valide !")
-        time.sleep(2)
-        clear()
-        choixUser()
-    
-    elif choixDeUser not in listeEntresValide:
-    #Si l'entrée de l'utilisateur n'est pas au bon format
-        if choixDeUser == "pierre" or choixDeUser == "p":
-            choixDeUser = "Pierre"
-        elif choixDeUser == "feuille" or choixDeUser == "f":
-            choixDeUser = "Feuille"
-        else: 
-            choixDeUser = "Ciseaux"
-
-    return choixDeUser
-
-#Fonction qui demande à l'utilisateur si il veut rejouer 
-def rejouer():
-    #On efface l'affichage du résultat 
-    time.sleep(2)
-    clear()
-
-    #On écrit le score dans un fichier txt
-    fichier = open("sauvegarde.txt", "w")
-    fichier.write(str(score)) #On oublie de convertir le score en string
-    fichier.close()
-
-    print("Votre score est actuellement de", str(score) +".")
-    time.sleep(2)
-    clear()
-
-    choix = input("Voulez vous rejouer ? (Oui|Non) \n>>> ")
-    clear()
-
-    if choix == "Oui" or  choix == "oui" or  choix == "o" or choix == "O":
-        jeu()
-
-    elif choix == "Non" or choix == "non" or choix == "n" or choix == "N":
-        print("Merci d'avoir joué, à bientôt !")
-        time.sleep(2)
-        clear()
-
-    else:
-        print("Votre choix n'est pas valide !")
-        rejouer()
+#On écrit le score dans un fichier txt
+fichier = open("sauvegarde.txt", "w")
+fichier.write(str(score)) #On oublie de convertir le score en string
+fichier.close()
 
 #Fonction qui contient le jeu en lui même
 def jeu():
     global score
-    #On inscrit le choix de l'user dans des variables afin qu'elle ne changent à chaque demande
-    choixDeUser = choixUser()
-    choixDeOrdi = choixOrdi()
 
     #En cas d'égalité
     if  choixDeUser == choixDeOrdi:
@@ -128,6 +111,4 @@ def jeu():
         print("L'ordinateur à choisi", choixDeOrdi +". L'ordinateur a gagné...")
         rejouer()
 
-jeu()
-
-
+fen.mainloop()
